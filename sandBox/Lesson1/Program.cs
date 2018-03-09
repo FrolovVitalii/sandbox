684578c5040460ca9d120d9fd92b4cb4da50ae79
FrolovVitalii <phoenixfvv@gmail.com>
2018-03-09 13:37:01 +0200

    Start new branch master-dev

---
+++ sandBox/Lesson1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Lesson1
+{
+    class Program
+    {
+        static string name = null;
+        static void Main()
+        {
+            Console.WriteLine("Please, enter your name.");
+
+            name = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine($"Hello, {name} !");
+            }
+            else
+            {
+                Console.WriteLine("You didn't write your name!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
