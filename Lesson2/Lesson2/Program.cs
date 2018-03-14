6f8f3fa92e7d6d1e853c25a896b247a6c180bc26
FrolovVitalii <phoenixfvv@gmail.com>
2018-03-14 19:14:35 +0200

    Add Lesson2.

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Program
+    {
+
+
+
+
+        public static int[] GetIntArray(object[] input)
+        {
+            List<int> listResult = new List<int>();
+
+            var intArray = input
+                                .Where(s => s is int)
+                                .Cast<int>()
+                                .ToArray();
+
+            Array.ForEach(
+                intArray, s => listResult
+                .Add(intArray
+                .Where((v, i) => i != (Array.IndexOf(intArray, s)))
+                .Aggregate((i, t) => i * t))
+                );
+
+
+            return listResult.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+
+            object[] array = { "e", 3, 2, "tt", 2.4, "1e", 1, 5, "q" };
+
+            Console.WriteLine("Current array:");
+            foreach (var item in array)
+            {
+                Console.Write(item + " :: ");
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("Result array:");
+            foreach (var item in GetIntArray(array))
+            {
+                Console.Write(item + " :: ");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
